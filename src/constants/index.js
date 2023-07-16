41bc5a229eb9b82788c99717e3490ed48bb9b3c9
Yassin-Aru <jocarhisouka@gmail.com>
2023-07-16 22:57:24 +0100

    another commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -176,7 +176,7 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      "I thought it was impossible to make a website as beautiful as our product, but Yassine proved me wrong.",
     name: "Sara Lee",
     designation: "CFO",
     company: "Acme Co",
@@ -184,7 +184,7 @@ const testimonials = [
   },
   {
     testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      "I've never met a web developer who truly cares about their clients' success like Yassine does.",
     name: "Chris Brown",
     designation: "COO",
     company: "DEF Corp",
@@ -192,7 +192,7 @@ const testimonials = [
   },
   {
     testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      "After Yassine optimized our website, our traffic increased by 50%. We can't thank them enough!",
     name: "Lisa Wang",
     designation: "CTO",
     company: "456 Enterprises",
