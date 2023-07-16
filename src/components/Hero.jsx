a082118f17775a04e99a80093923f82d9ea3fd1b
Yassin-Aru <jocarhisouka@gmail.com>
2023-07-16 20:53:35 +0100

    home ui btn

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => (
           </p>
           <div className='absolute xs:bottom-10 bottom-32 flex justify-left'>
             <a href='#about'>
-              <div className='w-[35px] h-[64px] rounded-3xl border-4 border-white flex justify-center items-start'>
+              <div className='w-[35px] h-[64px] rounded-3xl border-4 border-[#DFFF0F] flex justify-center items-start'>
                 <motion.div
                   animate={{
                     y: [0, 24, 0],
@@ -30,7 +30,7 @@ const Hero = () => (
                     repeat: Infinity,
                     repeatType: "loop",
                   }}
-                  className='w-3 h-3 rounded-full bg-white mb-1'
+                  className='w-3 h-3 rounded-full bg-[#DFFF0F] mb-1'
                 />
               </div>
             </a>
